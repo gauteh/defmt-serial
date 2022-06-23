7904f33ed245973f901c79e576da218b9ef10306
Gaute Hope <eg@gaute.vetsj.com>
2022-06-23 09:44:37 +0200

    various attempts

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,65 @@
 #![no_std]
 
-use core::ptr;
-use defmt::{global_logger, Write};
+use core::{marker::PhantomData, any::Any};
+use defmt::{global_logger, Logger};
+use embedded_hal::serial::{Error, ErrorKind, ErrorType, nb::Write};
 
-use crate::hal::{uart, prelude::*};
 
-pub struct UartLogger {
-    pub uart: uart::Uart0,
+static mut LOGGER: Option<*mut WriteWrapper> = None;
+static mut ENCODER: defmt::Encoder = defmt::Encoder::new();
+
+struct WriteWrapper(&'static dyn Write<u8, Error = dyn Error>);
+
+#[derive(Debug)]
+struct WWErr;
+
+impl ErrorType for WriteWrapper {
+    type Error = WWErr;
 }
 
-#[global_logger]
-pub struct UartGlobalLogger;
-
-unsafe impl defmt::Logger for UartGlobalLogger {
-    fn acquire() -> Option<ptr::NonNull<dyn Write>> {
-        unsafe {
-            LOGGER
-                .as_mut()
-                .map(|l| ptr::NonNull::new_unchecked(l as &mut dyn defmt::Write))
-        }
-    }
-
-    unsafe fn release(_writer: ptr::NonNull<dyn Write>) {}
-}
-
-impl defmt::Write for UartLogger {
-    fn write(&mut self, bytes: &[u8]) {
-        for b in bytes {
-            nb::block!(self.uart.write(*b)).ok();
-        }
+impl Error for WWErr {
+    fn kind(&self) -> ErrorKind {
+        ErrorKind::Other
     }
 }
 
-pub static mut LOGGER: Option<UartLogger> = None;
+impl Write<u8> for WriteWrapper {
+    fn write(&mut self, b: u8) -> nb::Result<(), Self::Error> {
+        self.0.write(b);
+        Ok(())
+    }
 
+    fn flush(&mut self) -> nb::Result<(), Self::Error> {
+        Ok(())
+    }
+}
+
+pub struct GlobalSerialLogger<W: Write<u8>>(PhantomData<W>);
+
+// #[global_logger]
+// pub struct UartGlobalLogger;
+
+unsafe impl<W: Write<u8>> defmt::Logger for GlobalSerialLogger<W> {
+    fn acquire() {
+        // check atomic bool if taken
+    }
+
+    unsafe fn release() {}
+
+    unsafe fn write(bytes: &[u8]) {
+    }
+
+    unsafe fn flush() {
+    }
+}
+
+fn write_serial(mut remaining: &[u8]) {
+    unsafe {
+        // if let Some(writer) = LOGGER {
+        //     for b in remaining {
+                // Write::<u8>::write(&mut *writer, *b);
+                // (*writer).write(*b);
+            // }
+        // }
+    }
+}
