6f30e83ba098ff057a396a11c47f9268ae24c998
Gaute Hope <eg@gaute.vetsj.com>
2022-06-23 10:59:06 +0200

    super unsafe

--- example-artemis/src/main.rs
+++ example-artemis/src/main.rs
@@ -4,14 +4,30 @@
 use panic_probe as _;
 use cortex_m::asm;
 use cortex_m_rt::entry;
-use ambiq_hal as hal;
+use ambiq_hal::{self as hal, prelude::*};
 use defmt;
 use defmt_rtt as _;
 
 #[entry]
 fn main() -> ! {
+    let mut dp = hal::pac::Peripherals::take().unwrap();
+    let core = hal::pac::CorePeripherals::take().unwrap();
+
+    let mut delay = hal::delay::Delay::new(core.SYST, &mut dp.CLKGEN);
+
+    let pins = hal::gpio::Pins::new(dp.GPIO);
+    let mut led = pins.d19.into_push_pull_output();
+
+    // set up serial
+    let mut serial = hal::uart::Uart0::new(dp.UART0, pins.tx0, pins.rx0);
+    // type Uart = hal::uart::Uart0;
+    defmt_serial::defmt_serial!(serial, hal::uart::Uart0);
+
     defmt::info!("Hello!");
+
     loop {
+        led.toggle().unwrap();
+        delay.delay_ms(2000u32);
         asm::nop();
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,37 @@
 #![no_std]
 
-use core::{marker::PhantomData, any::Any};
+use core::ffi::c_void;
+use core::ptr;
+use core::{any::Any, marker::PhantomData};
 use defmt::{global_logger, Logger};
-use embedded_hal::serial::{Error, ErrorKind, ErrorType, nb::Write};
+use embedded_hal::serial::{nb::Write, Error, ErrorKind, ErrorType};
 
+static mut ENCODER: defmt::Encoder = defmt::Encoder::new();
+pub static mut WRITEFN: *const c_void = ptr::null_mut();
+
+#[macro_export]
+macro_rules! defmt_serial {
+    ($serial:ident, $stype:ty) => {
+        use core::ptr;
+        static mut LOGGER: *mut $stype = ptr::null_mut();
+
+        let wfn = |bytes: &[u8]| {
+            unsafe {
+                for b in bytes {
+                    (*LOGGER).write(*b);
+                }
+            }
+        };
+
+        unsafe {
+            LOGGER = &mut ($serial) as *mut _;
+
+            defmt_serial::WRITEFN = &wfn as *const _ as *const c_void;
+        }
+    };
+}
 
 static mut LOGGER: Option<*mut WriteWrapper> = None;
-static mut ENCODER: defmt::Encoder = defmt::Encoder::new();
 
 struct WriteWrapper(&'static dyn Write<u8, Error = dyn Error>);
 
@@ -23,9 +48,14 @@ impl Error for WWErr {
     }
 }
 
+static mut ERR_ARR: [u8; 124] = [0u8; 124];
+
 impl Write<u8> for WriteWrapper {
     fn write(&mut self, b: u8) -> nb::Result<(), Self::Error> {
-        self.0.write(b);
+        unsafe {
+            // ERR_ARR = core::mem::transmute_copy(&self.0.write(b));
+            // ERR_ARR = self.0.write(b) as *const _;
+        }
         Ok(())
     }
 
@@ -46,20 +76,18 @@ unsafe impl<W: Write<u8>> defmt::Logger for GlobalSerialLogger<W> {
 
     unsafe fn release() {}
 
-    unsafe fn write(bytes: &[u8]) {
-    }
+    unsafe fn write(bytes: &[u8]) {}
 
-    unsafe fn flush() {
-    }
+    unsafe fn flush() {}
 }
 
 fn write_serial(mut remaining: &[u8]) {
     unsafe {
         // if let Some(writer) = LOGGER {
         //     for b in remaining {
-                // Write::<u8>::write(&mut *writer, *b);
-                // (*writer).write(*b);
-            // }
+        // Write::<u8>::write(&mut *writer, *b);
+        // (*writer).write(*b);
+        // }
         // }
     }
 }
